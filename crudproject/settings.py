807e809ce57bd95973a05db99b3c60e978d5b693
LEE <mj960704@gmail.com>
2019-06-01 00:57:24 +0900

    haha

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6c73%9qag&^lt3=5s1cd*h(m*2%tx+yg&dbo9uk9)8veuixeu#'
+# SECRET_KEY = '6c73%9qag&^lt3=5s1cd*h(m*2%tx+yg&dbo9uk9)8veuixeu#'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6c73%9qag&^lt3=5s1cd*h(m*2%tx+yg&dbo9uk9)8veuixeu#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
